d26066dbc1817ae4df60b5febc4529b4130efcc1
ruhi89 <ruhi.barua89@gmail.com>
2025-06-03 15:53:10 +0600

    Initial commit

---
+++ SysStock/LoginForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace SysStock
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ShowPassCB = new System.Windows.Forms.CheckBox();
+            this.Loginbtn = new System.Windows.Forms.Button();
+            this.Clearbtn = new System.Windows.Forms.Button();
+            this.Usernametxt = new System.Windows.Forms.TextBox();
+            this.Passtxt = new System.Windows.Forms.TextBox();
+            this.Usernamelbl = new System.Windows.Forms.Label();
+            this.Passlbl = new System.Windows.Forms.Label();
+            this.Loginlbl = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ShowPassCB);
+            this.panel1.Controls.Add(this.Loginbtn);
+            this.panel1.Controls.Add(this.Clearbtn);
+            this.panel1.Controls.Add(this.Usernametxt);
+            this.panel1.Controls.Add(this.Passtxt);
+            this.panel1.Controls.Add(this.Usernamelbl);
+            this.panel1.Controls.Add(this.Passlbl);
+            this.panel1.Controls.Add(this.Loginlbl);
+            this.panel1.Location = new System.Drawing.Point(378, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(338, 370);
+            this.panel1.TabIndex = 0;
+            // 
+            // ShowPassCB
+            // 
+            this.ShowPassCB.AutoSize = true;
+            this.ShowPassCB.Font = new System.Drawing.Font("Segoe UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShowPassCB.Location = new System.Drawing.Point(143, 218);
+            this.ShowPassCB.Name = "ShowPassCB";
+            this.ShowPassCB.Size = new System.Drawing.Size(115, 21);
+            this.ShowPassCB.TabIndex = 9;
+            this.ShowPassCB.Text = "Show Password";
+            this.ShowPassCB.UseVisualStyleBackColor = true;
+            this.ShowPassCB.CheckedChanged += new System.EventHandler(this.ShowPassCB_CheckedChanged);
+            // 
+            // Loginbtn
+            // 
+            this.Loginbtn.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.Loginbtn.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Loginbtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.Loginbtn.Location = new System.Drawing.Point(177, 260);
+            this.Loginbtn.Name = "Loginbtn";
+            this.Loginbtn.Size = new System.Drawing.Size(125, 35);
+            this.Loginbtn.TabIndex = 8;
+            this.Loginbtn.Text = "Login";
+            this.Loginbtn.UseVisualStyleBackColor = false;
+            this.Loginbtn.Click += new System.EventHandler(this.Loginbtn_Click);
+            // 
+            // Clearbtn
+            // 
+            this.Clearbtn.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.Clearbtn.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clearbtn.Location = new System.Drawing.Point(44, 260);
+            this.Clearbtn.Name = "Clearbtn";
+            this.Clearbtn.Size = new System.Drawing.Size(125, 35);
+            this.Clearbtn.TabIndex = 7;
+            this.Clearbtn.Text = "Clear";
+            this.Clearbtn.UseVisualStyleBackColor = false;
+            this.Clearbtn.Click += new System.EventHandler(this.Clearbtn_Click);
+            // 
+            // Usernametxt
+            // 
+            this.Usernametxt.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Usernametxt.Location = new System.Drawing.Point(143, 118);
+            this.Usernametxt.Name = "Usernametxt";
+            this.Usernametxt.Size = new System.Drawing.Size(158, 29);
+            this.Usernametxt.TabIndex = 0;
+            // 
+            // Passtxt
+            // 
+            this.Passtxt.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Passtxt.Location = new System.Drawing.Point(143, 183);
+            this.Passtxt.Name = "Passtxt";
+            this.Passtxt.PasswordChar = '*';
+            this.Passtxt.Size = new System.Drawing.Size(158, 29);
+            this.Passtxt.TabIndex = 5;
+            // 
+            // Usernamelbl
+            // 
+            this.Usernamelbl.AutoSize = true;
+            this.Usernamelbl.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Usernamelbl.Location = new System.Drawing.Point(40, 124);
+            this.Usernamelbl.Name = "Usernamelbl";
+            this.Usernamelbl.Size = new System.Drawing.Size(81, 21);
+            this.Usernamelbl.TabIndex = 4;
+            this.Usernamelbl.Text = "Username";
+            // 
+            // Passlbl
+            // 
+            this.Passlbl.AutoSize = true;
+            this.Passlbl.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Passlbl.Location = new System.Drawing.Point(40, 186);
+            this.Passlbl.Name = "Passlbl";
+            this.Passlbl.Size = new System.Drawing.Size(76, 21);
+            this.Passlbl.TabIndex = 3;
+            this.Passlbl.Text = "Password";
+            // 
+            // Loginlbl
+            // 
+            this.Loginlbl.AutoSize = true;
+            this.Loginlbl.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Loginlbl.Location = new System.Drawing.Point(138, 49);
+            this.Loginlbl.Name = "Loginlbl";
+            this.Loginlbl.Size = new System.Drawing.Size(70, 30);
+            this.Loginlbl.TabIndex = 0;
+            this.Loginlbl.Text = "Login";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SysStock.Properties.Resources.hero_img;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(338, 370);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(728, 394);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "LoginForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SysStock";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Loginlbl;
+        private System.Windows.Forms.Label Usernamelbl;
+        private System.Windows.Forms.Label Passlbl;
+        private System.Windows.Forms.Button Loginbtn;
+        private System.Windows.Forms.Button Clearbtn;
+        private System.Windows.Forms.TextBox Usernametxt;
+        private System.Windows.Forms.TextBox Passtxt;
+        private System.Windows.Forms.CheckBox ShowPassCB;
+    }
+}
+
---
+++ SysStock/LoginForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SysStock
+{
+    public partial class LoginForm: Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            Usernametxt.Focus();
+        }
+        private void Loginbtn_Click(object sender, EventArgs e)
+        {
+            string username = Usernametxt.Text.Trim();
+            string password = Passtxt.Text;
+
+            // Example: Hardcoded login credentials
+            if (username == "admin" && password == "1234")
+            {
+                MessageBox.Show("Login Successful!", "SysStock", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Open MainForm
+                //MainForm mainForm = new MainForm();
+                //this.Hide(); // Hide Login Form
+                //mainForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Usernametxt.Clear();
+                Passtxt.Clear();
+                Usernametxt.Focus();
+            }
+        }
+
+        private void Clearbtn_Click(object sender, EventArgs e)
+        {
+            Usernametxt.Clear();
+            Passtxt.Clear();
+            Usernametxt.Focus();
+        }
+
+        private void ShowPassCB_CheckedChanged(object sender, EventArgs e)
+        {
+            if(ShowPassCB.Checked)
+            {
+                Passtxt.PasswordChar = '\0';
+            }
+            else
+            {
+                Passtxt.PasswordChar = '*';
+            }
+        }
+    }
+}
